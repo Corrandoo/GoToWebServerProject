fef5eebf7e81b85b54e0fa40ae6fe0b640118f8e
Corrandoo <ivan260302@yandex.ru>
2017-01-06 11:43:34 +0300

    First work

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import tornado.ioloop
 import tornado.web
 
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("<h1>Hello world!</h1>")
+
+settings = [
+    ('/', MainHandler)
+]
+
 app = tornado.web.Application([])
 app.listen(8888)
 tornado.ioloop.IOLoop.current().start()
