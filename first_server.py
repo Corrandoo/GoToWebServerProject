e7ba1949765f7941d544a0d7720bae0f5b6f5ed5
Corrandoo <ivan260302@yandex.ru>
2017-01-06 12:53:47 +0300

    Added CatsHandler

--- first_server.py
+++ first_server.py
@@ -1,18 +1,34 @@
 import tornado.ioloop
 import tornado.web
+import random
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("<h1>Hello world!</h1>")
+        self.write("<form method ='get' action ='/hello'><input name='name' /></form>")
 
 class HelloHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("<h1>Привет, мой Господин!</h1>")
+        name = self.get_argument("name", "Анон")
+        self.write("<h1>Привет, " + name + "</h1>")
+
+
+class CatsHandler(tornado.web.RequestHandler):
+    def get(self):
+        cats = [
+            ('Васька', 'http://img1.liveinternet.ru/images/attach/c/5/86/352/86352251_samuye_krasivuye_fotografii_kotov__4_.jpg'),
+            ('Петька', 'http://vesti.dp.ua/wp-content/uploads/2016/09/%D0%BA%D0%BE%D1%82%D1%8B.jpg'),
+
+        ]
+        cat = random.choice(cats)
+        self.render("cats.html", cat_name=cat[0], cat_adress=cat[1])
+
 
 settings = [
     ('/', MainHandler),
-    ('/hello', HelloHandler)
+    ('/hello', HelloHandler),
+    ('/cats', CatsHandler)
 ]
+# http://host:port/page/page?param=value&param1=value
 
 app = tornado.web.Application(settings)  # создали веб приложение
 app.listen(8888)  # слушаем 8888 порт
